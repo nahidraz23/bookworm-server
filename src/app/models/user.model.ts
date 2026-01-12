8b08949262b08b50a13724fb2e6cb044d2363913
nahidraz23 <nzamanraz@gmail.com>
2026-01-12 15:46:37 +0600

    feat: add user model and password hashing

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+import { Iuser } from "../interfaces/user.interface";
+
+const userSchema = new Schema<Iuser> ({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    photo: {type: String},
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+},
+{
+    timestamps: true,
+    versionKey:false
+}
+)
+
+export const User = model<Iuser>('User', userSchema);
